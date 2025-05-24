6bb1b7e4621eaac81b5c7366034a6549371cec48
Ray <ray442748@gmail.com>
2025-05-25 00:48:47 +0100

    Added intermediate models and marts, along with refining comments in the dim_products and fact_orders tables. data-dungeon-warehouse v1 complete.

--- dimensions/dim_products.sql
+++ dimensions/dim_products.sql
@@ -1,3 +1,6 @@
+
+-- This SQL script creates the dim_products table and populates it with product data from the staging table stg_products.
+
 CREATE TABLE dim_products (
 	product_code CHAR(4) PRIMARY KEY CHECK (product_code ~ '^[A-Z0-9]{4}$'),
 	product_name TEXT,
@@ -10,3 +13,12 @@ CREATE TABLE dim_products (
 );
 
 -- Added  created and updated timestamp columns to track changes over time for auditing purposes
+
+INSERT INTO dim_products (product_code, product_name, cost, is_available, avail_needs_review)
+SELECT
+	product_code,
+	product_name,
+	cost,
+	is_available,
+	avail_needs_review
+FROM stg_products;
--- facts/fact_orders.sql
+++ facts/fact_orders.sql
@@ -1,3 +1,5 @@
+-- This SQL script populates the fact_orders table from the staging table stg_orders.
+-- It includes foreign key constraints to ensure referential integrity with the dimension tables.
 CREATE TABLE fact_orders (
     order_id SERIAL PRIMARY KEY,
     customer_id INTEGER NOT NULL,
@@ -15,3 +17,13 @@ CREATE TABLE fact_orders (
     CONSTRAINT fk_order_status
         FOREIGN KEY (order_status_id) REFERENCES dim_order_status(order_status_id)
 );
+
+INSERT INTO fact_orders (customer_id, product_code, order_date, qty, order_status, order_status_id)
+SELECT 
+    customer_id,
+    product_code,
+    order_date,
+    qty,
+    order_status,
+    order_status_id
+FROM stg_orders;
